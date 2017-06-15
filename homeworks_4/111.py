e2a6daefd6cdc4c0733033b2f7bad3010dbd8343
krivbas <kucher@mymail.in.ua>
2017-06-15 12:23:08 +0300

    Replace homework

--- homeworks_4/111.py
+++ homeworks_4/111.py
@@ -3,20 +3,31 @@ my_dict = {"first_name":"Nikola",
            "email":"test@mymail.in.ua",
            "age": 29}
 
-print(my_dict.items())
-
-my_dict2 = list(my_dict)
+my_dict2 = dict.values(my_dict)
 
 print(my_dict2)
 
 my_dict3 = []
 
-for mylist in my_dict2:
-    if isinstance(mylist, str):
-        if mylist.find("a") and len(mylist) > 5:
-            my_dict3.append(mylist)
-        else:
-            print("no ok")
+for value in my_dict2:
+    if isinstance(value, int):
+        if 25 <= value <= 40:
+            my_dict3.append(str(value))
     else:
-        print("no ok2")
+        if len(value) > 5 and value.find("a") >= 0:
+            my_dict3.append(value)
 print(my_dict3)
+
+if value in my_dict3:
+    if value.find("n") >= 0 or value.find("m") >= 0:
+        my_dict3.remove(value)
+print(my_dict3)
+
+my_dict3.sort(reverse=True)
+print(my_dict3)
+
+my_dict4 = ",".join(my_dict3)
+print(my_dict4)
+
+my_dict5 = my_dict4.split(",")
+print(my_dict5)
